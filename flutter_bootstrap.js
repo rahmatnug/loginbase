3f55f407f673380c84eaabd67612372ecde57426
rahmatnug <rahmatnugrohosaputra@gmail.com>
2025-10-12 21:15:09 +0700

    new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "432336708"
+    serviceWorkerVersion: "2420723517"
   }
 });
